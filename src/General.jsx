3dec3839c857aef29195ee2de8af5318dd406de1
Liam <liamsuu14974@gmail.com>
2024-07-09 20:42:46 +0100

    Create general inputs component

---
+++ src/General.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+export default function General() {
+  const [generalInfo, setGeneralInfo] = useState({
+    fullName: "",
+    email: "",
+    phone: 0,
+  });
+
+  function handleName(e) {
+    setGeneralInfo({ ...generalInfo, fullName: e.target.value });
+  }
+  function handleEmail(e) {
+    setGeneralInfo({ ...generalInfo, email: e.target.value });
+  }
+  function handlePhone(e) {
+    setGeneralInfo({ ...generalInfo, phone: e.target.value });
+  }
+  return (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <label>
+        Full Name:
+        <input
+          type="text"
+          id="name"
+          value={generalInfo.fullName}
+          onChange={handleName}
+        />
+      </label>
+      <label>
+        Email Address(example@example.com) :
+        <input
+          type="email"
+          id="email"
+          value={generalInfo.email}
+          onChange={handleEmail}
+        />
+      </label>
+      <label>
+        Phone number:
+        <input
+          type="phone"
+          id="phone"
+          value={generalInfo.phone}
+          onChange={handlePhone}
+        />
+      </label>
+    </div>
+  );
+}
